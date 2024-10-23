1f242b5825a95b19cbd85b0fd735aa4be9771d88
James Tang <juntjtang@gmail.com>
2024-10-23 00:30:22 -0700

    empty project

---
+++ src/main/java/com/fdu/msacs/dfs/dfs_shared/DfsSharedApplication.java
@@ -0,0 +1,13 @@
+package com.fdu.msacs.dfs.dfs_shared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsSharedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfsSharedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdu/msacs/dfs/dfs_shared/DfsSharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdu.msacs.dfs.dfs_shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfsSharedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
